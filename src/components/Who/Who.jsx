97b7bfafd27e393d2c2bd4e04d0d072ec3b84379
Jenny Patel <pateljenny555@gmail.com>
2023-05-07 14:36:02 -0700

    Created responsive design for Who Component

--- src/components/Who/Who.jsx
+++ src/components/Who/Who.jsx
@@ -25,15 +25,28 @@ const Content = styled.div`
   flex-direction: column;
   justify-content: center;
   gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+      align-items: center;
+      text-align: center;
+  }
 `
 
 const Design = styled.div`
   flex: 1;
   position: relative;
+
+  @media only screen and (max-width: 768px) {
+      display: none;
+  }
 `
 
 const Title = styled.h1`
   font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+  }
 `
 
 const WhatWeDo = styled.div`
@@ -65,7 +78,6 @@ const Button = styled.button`
   border-radius: 5px;
   cursor: pointer;
 `
-
 
 const Who = () => {
   return (
