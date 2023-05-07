fe41293c75e238ad441cd397acc4efff8874d3d2
Jenny Patel <pateljenny555@gmail.com>
2023-05-07 11:53:11 -0700

    Created distorted sphear arount the hero image.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
 import Navbar from './Navbar';
+import { Canvas } from "@react-three/fiber"
+import { MeshDistortMaterial, OrbitControls, Sphere } from '@react-three/drei'
 
 const Section = styled.div`
   height: 100vh;
@@ -98,6 +100,14 @@ const Hero = () => {
           <Button>Learn More</Button>
         </Content>
         <Design>
+          <Canvas>
+            <OrbitControls enableZoom={false} autoRotate />
+            <ambientLight intensity={1.5} />
+            <directionalLight position={[3, 2, 1]} />
+            <Sphere args={[1, 100, 200]} scale={2.4}>
+              <MeshDistortMaterial color="#3d1c56" attach="material" distort={0.5} speed={2} />
+            </Sphere>
+          </Canvas>
           <Img src="./img/moon.png" />
         </Design>
       </Container>
