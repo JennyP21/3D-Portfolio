38f8274c3d13b762c922ac9a07aee0125f2e5c11
Jenny Patel <pateljenny555@gmail.com>
2023-05-07 14:46:44 -0700

    Created Reponsive Design for Contact Page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -23,6 +23,10 @@ const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
 `
 
 const Title = styled.h1`
@@ -34,6 +38,10 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 25px;
+
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+  }
 `
 
 const Input = styled.input`
@@ -62,6 +70,10 @@ const Button = styled.button`
 
 const Design = styled.div`
   flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 `
 
 const Contact = () => {
