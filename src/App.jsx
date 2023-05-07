682d975b8d0f93ff5f2cf862ca283b802702d03c
Jenny Patel <pateljenny555@gmail.com>
2023-05-07 10:50:17 -0700

    Created a Test environment for 3d Objects

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Who from "./components/Who"
 import Works from "./components/Works"
 import Contact from "./components/Contact"
+import Test from "./components/Test"
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -26,6 +27,7 @@ function App() {
       <Who />
       <Works />
       <Contact />
+      <Test />
     </Container>
   )
 }
---
+++ src/components/Test.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from '@react-three/drei'
+import Cube from './Cube'
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 100%;
+`
+
+const Test = () => {
+    return (
+        <Container>
+            <Canvas>
+                <OrbitControls enableZoom={false} autoRotate />
+                <ambientLight intensity={1} />
+                <directionalLight position={[3, 2, 1]} />
+                <Cube />
+            </Canvas>
+        </Container>
+    )
+}
+
+export default Test
